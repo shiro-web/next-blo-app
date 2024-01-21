e9f903775bb35338a5cac65d223be39bfc86f754
shiro-web <shiro-kwsm-46@docomo.ne.jp>
2024-01-21 19:16:33 +0900

    ✨ HTML,CSS完成

---
+++ src/app/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='py-2 px-2 border-t'>
+        <small>@2023 hoge</small>
+    </footer>
+  )
+}
+
+export default Footer
